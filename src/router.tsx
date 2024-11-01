98fe65e2deafe6041009c6428a197af73e7c6849
DESKTOP-A742PF6\default <jinseok1006@naver.com>
2024-11-01 16:57:19 +0900

    라우팅 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,29 +1,24 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Outlet,
   Route,
 } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage";
 import { ROUTES } from "./constants";
 import WishListPage from "./pages/WishListPage";
+import PopularPage from "./pages/PopularPage";
+import SearchPage from "./pages/SearchPage";
+import Layout from "./components/Layout";
 
-const Layout = () => {
-  return (
-    <>
-      {/* <Navbar /> */}
-      <Outlet />
-      {/* <Footer /> */}
-    </>
-  );
-};
 
 const routes = createRoutesFromElements(
   <Route path={ROUTES.root} element={<Layout />}>
     <Route index element={<HomePage />} />
     <Route path={ROUTES.signin} element={<SignInPage />} />
     <Route path={ROUTES.wishlist} element={<WishListPage />} />
+    <Route path={ROUTES.popular} element={<PopularPage />} />
+    <Route path={ROUTES.search} element={<SearchPage />} />
   </Route>
 );
 
