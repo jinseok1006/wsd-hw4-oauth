3607f4d9a5214dc02d001095d68f73baa6be72bf
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-11-24 22:49:30 +0900

    페이지 등장시 애니메이션 지정

---
+++ src/animation/pageTransition.ts
@@ -0,0 +1,24 @@
+export const pageTransition = {
+  initial: { opacity: 0, y: -50 }, // 초기 위치 (위로 50px 이동)
+  animate: { opacity: 1, y: 0 }, // 애니메이션 중 위치 (원래 위치)
+  exit: { opacity: 0, y: 50 }, // 종료 위치 (아래로 50px 이동)
+  transition: { duration: 0.5 }, // 전환 시간 (0.5초)
+};
+
+export const fadeInAnimation = {
+  initial: { opacity: 0, y: 40 }, // 시작 상태: 약간 아래에서 투명
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } }, // 애니메이션: 불투명 + 원위치
+};
+
+export const fadeInCommonOptions = {
+  variants: fadeInAnimation,
+  initial: "initial",
+  whileInView: "animate",
+  viewport: { once: true },
+};
+
+export const fadeInInfiniteScroll = {
+  initial: { opacity: 0, y: 20 }, // 시작 상태: 약간 아래에서 투명
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } }, // 애니메이션: 불투명 + 원위치
+};
+
--- src/components/MoviePosterInf.tsx
+++ src/components/MoviePosterInf.tsx
@@ -1,7 +1,10 @@
 import useWishlistStore from "../store/useWishlistStore";
 import { Movie, TMDB_IMAGE } from "../api";
 import { Box, Typography } from "@mui/material";
+import { motion } from "motion/react";
+import { fadeInCommonOptions } from "../animation/pageTransition";
 
+const motionDivStyle = { height: "100%" };
 export default function MoviePosterInf({
   movie,
   width,
@@ -16,58 +19,59 @@ export default function MoviePosterInf({
   const isWishlisted = includeWishlist(movie);
 
   return (
-    <Box
-      onClick={() => toggleWishlist(movie)}
-      sx={{
-        height: "100%",
-        position: "relative",
-        transition: "transform 0.5s ease",
-        ":hover": {
-          transform: `scale(1.05)`,
-        },
-      }}
-    >
+    <motion.div style={motionDivStyle} {...fadeInCommonOptions}>
       <Box
-        component="img"
-        src={`${TMDB_IMAGE}/w300/${movie.poster_path}`}
-        alt={movie.title}
-        sx={[
-          {
-            width: "100%",
-            height: "auto",
-            objectFit: "cover",
-            borderRadius: 1,
+        onClick={() => toggleWishlist(movie)}
+        sx={{
+          height: "100%",
+          position: "relative",
+          transition: "transform 0.5s ease",
+          ":hover": {
+            transform: `scale(1.05)`,
           },
-          { width, height },
-        ]}
-      />
-      <Typography
-        variant="subtitle1"
-        align="center"
-        textOverflow="ellipsis"
-        overflow="hidden"
-        whiteSpace="nowrap"
-        width={width}
-      
+        }}
       >
-        {movie.title}
-      </Typography>
-      {isWishlisted ? (
         <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            color: "white",
-            background: "black",
-            padding: 1,
-            opacity: 0.7,
-            borderRadius: 1,
-          }}
+          component="img"
+          src={`${TMDB_IMAGE}/w300/${movie.poster_path}`}
+          alt={movie.title}
+          sx={[
+            {
+              width: "100%",
+              height: "auto",
+              objectFit: "cover",
+              borderRadius: 1,
+            },
+            { width, height },
+          ]}
+        />
+        <Typography
+          variant="subtitle1"
+          align="center"
+          textOverflow="ellipsis"
+          overflow="hidden"
+          whiteSpace="nowrap"
+          width={width}
         >
-          ❤️
-        </Box>
-      ) : null}
-    </Box>
+          {movie.title}
+        </Typography>
+        {isWishlisted ? (
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              color: "white",
+              background: "black",
+              padding: 1,
+              opacity: 0.7,
+              borderRadius: 1,
+            }}
+          >
+            ❤️
+          </Box>
+        ) : null}
+      </Box>
+    </motion.div>
   );
 }
