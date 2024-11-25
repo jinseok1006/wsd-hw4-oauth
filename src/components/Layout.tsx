52254bed7a971c37e46c4f82d8703d4beff2de88
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-11-25 23:17:57 +0900

    라우팅 변경시 최상단으로 이동

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Appbar from "./Appbar";
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Appbar />
