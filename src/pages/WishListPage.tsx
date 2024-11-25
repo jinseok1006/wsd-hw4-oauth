fa601ff95d0cb7cedf0dc3dd75441036fe8f471a
DESKTOP-T9LEV0V\oslab <jinseok1006@naver.com>
2024-11-25 20:25:39 +0900

    위시리스트 빈값시 출력

--- src/pages/WishListPage.tsx
+++ src/pages/WishListPage.tsx
@@ -2,12 +2,12 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Grid from "@mui/material/Grid2";
 import MoviePosterInf from "../components/MoviePosterInf";
 import ScrollTop from "../components/ScrollTop";
-import { Container, Fab } from "@mui/material";
+import { Box, Container, Fab, Typography } from "@mui/material";
 import { KeyboardArrowUp as KeyboardArrowUpIcon } from "@mui/icons-material";
 import useWishlistStore from "../store/useWishlistStore";
 import { AnimatePresence } from "motion/react";
-
-
+import { motion } from "motion/react";
+import { fadeInCommonOptions } from "../animation/pageTransition";
 // const scrollStyle={
 //   marginT
 // }
@@ -25,11 +25,24 @@ export default function WishListPage() {
         >
           <Grid container spacing={2} sx={{ py: 2, px: 2 }}>
             <AnimatePresence>
-              {wishlist.map((movie ) => (
-                <Grid size={{ xs: 4, sm: 3, md: 1.5 }} key={movie.poster_path}>
-                  <MoviePosterInf movie={movie} />
-                </Grid>
-              ))}
+              {wishlist.length === 0 ? ( 
+                 <Grid size={{ xs: 12 }}>
+                   <motion.div {...fadeInCommonOptions}>
+                     <Typography textAlign="center">
+                       저장된 영화가 없습니다.
+                     </Typography>
+                   </motion.div>
+                 </Grid>
+               ) : (
+                 wishlist.map((movie) => (
+                   <Grid
+                     size={{ xs: 4, sm: 3, md: 1.5 }}
+                     key={movie.poster_path}
+                   >
+                     <MoviePosterInf movie={movie} />
+                   </Grid>
+                 ))
+               )}
             </AnimatePresence>
           </Grid>
         </InfiniteScroll>
