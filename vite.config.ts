ff978662250b56e371e1c5f0adb05e913f635a94
jinseok1006 <jinseok1006@naver.com>
2024-12-26 00:45:13 +0900

    베이스 경로변경

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(() => {
     //   // 환경 변수를 클라이언트 코드에서 사용 가능하도록 정의
     //   'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
     // },
-    base: "/wsd-hw2-fe/",
+    base: "/wsd-hw4-oauth/",
     plugins: [react()],
     server: {
       host: true,
